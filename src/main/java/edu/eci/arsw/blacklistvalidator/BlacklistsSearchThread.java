c5bb2fcf444c4f4bf7d9e8f87fabd61bff5bfe76
Manuel Felipe Barrera Barrera <felipebarrera104@gmail.com>
2025-02-27 08:29:25 -0500

    Add files via upload

---
+++ src/main/java/edu/eci/arsw/blacklistvalidator/BlacklistsSearchThread.java
@@ -0,0 +1,35 @@
+package edu.eci.arsw.blacklistvalidator;
+
+import java.util.List;
+
+import edu.eci.arsw.spamkeywordsdatasource.HostBlacklistsDataSourceFacade;
+
+public class BlacklistsSearchThread extends Thread {
+    private HostBlacklistsDataSourceFacade skds;
+    private String ipaddress;
+    private int startIndex, endIndex;
+    private List<Integer> blackListOcurrences;
+    private SharedCounter sharedCounter;
+
+    public BlacklistsSearchThread (String ipaddress, HostBlacklistsDataSourceFacade skds, int startIndex, int endIndex, List<Integer> blackListOcurrences, SharedCounter sharedCounter){
+        this.ipaddress = ipaddress;
+        this.skds = skds;
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+        this.blackListOcurrences = blackListOcurrences;
+        this.sharedCounter = sharedCounter;
+    }
+    @Override
+
+    public void run(){
+        for (int i = startIndex; i < endIndex && SharedCounter.get() < HostBlackListsValidator.BLACK_LIST_ALARM_COUNT; i++){
+            if(skds.isInBlackListServer(i, ipaddress)) {
+                synchronized (blackListOcurrences){
+                    blackListOcurrences.add(i);
+                }
+                sharedCounter.increment();
+            }
+
+        }
+    }
+}
---
+++ src/main/java/edu/eci/arsw/blacklistvalidator/SharedCounter.java
@@ -0,0 +1,15 @@
+package edu.eci.arsw.blacklistvalidator;
+
+public class SharedCounter {
+
+    public static int get() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    public void increment() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'increment'");
+    }
+    
+}
